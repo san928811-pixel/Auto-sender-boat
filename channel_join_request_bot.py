e05d7827531f10425d9f3966836120236131a0ff
san928811-pixel <san928811@gmail.com>
2025-12-14 10:20:20 +0530

    Update channel_join_request_bot.py

--- channel_join_request_bot.py
+++ channel_join_request_bot.py
@@ -26,7 +26,7 @@ WELCOME_TEMPLATE = (
     "https://t.me/+6BDthPVQV041NjFk\n\n"
 
     "⭐ Open Video Collection\n"
-    "https://t.me/+S-u7h8g2rxE5MTQ0\n\n"
+    "https://t.me/+mV7IArPBHYgyYzM0\n\n"
 
     "⭐ Premium Video\n"
     "https://t.me/ruhi_roy_01?text=primium_membership_collection🥵\n"

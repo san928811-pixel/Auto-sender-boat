aa41450184e8e1f9ab4e682334cb9385df0a04d6
san928811-pixel <san928811@gmail.com>
2025-12-17 21:12:58 +0530

    Update channel_join_request_bot.py

--- channel_join_request_bot.py
+++ channel_join_request_bot.py
@@ -26,7 +26,7 @@ WELCOME_TEMPLATE = (
     "https://t.me/+_z12fStYCckzZWE0\n\n"
 
     "⭐ Open Video Collection\n"
-    "https://t.me/+r8vPqMOseP1kY2Y0\n\n"
+    "https://t.me/+6LuFjPOyAG84ZGI8\n\n"
 
     "⭐ Premium Video\n"
     "https://t.me/ruhi_roy_01?text=primium_membership_collection🥵\n"
